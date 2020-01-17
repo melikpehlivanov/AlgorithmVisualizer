b01e925742d6c0106a763b1947a34cd451a27386
Melik Pehlivanov <melikpehlivanov@gmail.com>
2020-01-17 03:16:56 +0200

    Extract grid constants in gridConstants.js

---
+++ AlgoVisualizer/AlgoVisualizer/ClientApp/src/constants/gridConstants.js
@@ -0,0 +1,12 @@
+export const ROWS = 21;
+export const COLS = 60;
+
+export const START_NODE_ROW = 5;
+export const START_NODE_COL = 15;
+export const END_NODE_ROW = 10;
+export const END_NODE_COL = 35;
+
+export const SET_GRID = 'SET_GRID';
+export const SET_ALGORITHM = 'SET_ALGORITHM';
+export const SET_ALGORITHM_DESCRIPTION = 'SET_ALGORITHM_DESCRIPTION';
+export const CLEAR_STATE = 'CLEAR_STATE';
