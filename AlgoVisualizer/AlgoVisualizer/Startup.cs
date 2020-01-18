f530a1e5e7b9d44fc552b2e56736d7db5038b229
Melik Pehlivanov <melikpehlivanov@gmail.com>
2020-01-18 11:20:07 +0200

    Move usings inside namespace

--- AlgoVisualizer/AlgoVisualizer/Startup.cs
+++ AlgoVisualizer/AlgoVisualizer/Startup.cs
@@ -1,13 +1,10 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-
 namespace AlgoVisualizer
 {
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
     using Microsoft.Net.Http.Headers;
 
     public class Startup
