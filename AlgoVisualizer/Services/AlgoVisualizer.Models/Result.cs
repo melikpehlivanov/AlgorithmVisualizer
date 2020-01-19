a2e913947bf9978a8f13af99bb121f100a10ab98
Melik Pehlivanov <melikpehlivanov@gmail.com>
2020-01-19 21:04:34 +0200

    Add Result service model

---
+++ AlgoVisualizer/Services/AlgoVisualizer.Models/Result.cs
@@ -0,0 +1,17 @@
+﻿namespace AlgoVisualizer.Services.Models
+{
+    using System.Collections.Generic;
+
+    public class Result
+    {
+        public Result(IEnumerable<INode> allVisitedNodesInOrder, IEnumerable<INode> allNodesInShortestPathOrder = null)
+        {
+            this.AllVisitedNodesInOrder = allVisitedNodesInOrder;
+            this.AllNodesInShortestPathOrder = allNodesInShortestPathOrder;
+        }
+
+        public IEnumerable<INode> AllVisitedNodesInOrder { get; }
+
+        public IEnumerable<INode> AllNodesInShortestPathOrder { get; }
+    }
+}
