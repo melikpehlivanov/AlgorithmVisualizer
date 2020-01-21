a23d56812b4d6e335d815fe2e103e136e6aed549
Melik Pehlivanov <melikpehlivanov@gmail.com>
2020-01-21 22:44:15 +0200

    Implement AStarServiceModel and AStarNode in Services/AlgoVisualizer.Models/AStar

---
+++ AlgoVisualizer/Services/AlgoVisualizer.Models/AStar/AStarServiceModel.cs
@@ -0,0 +1,16 @@
+﻿namespace AlgoVisualizer.Models.AStar
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class AStarServiceModel
+    {
+        [Required]
+        public AStarNode StartNode { get; set; }
+
+        [Required]
+        public AStarNode EndNode { get; set; }
+
+        [Required]
+        public AStarNode[,] Grid { get; set; }
+    }
+}
