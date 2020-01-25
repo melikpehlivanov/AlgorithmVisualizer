669ffcbe3705f7f0de38ea153a9838e59a27313f
Melik Pehlivanov <melikpehlivanov@gmail.com>
2020-01-25 18:33:50 +0200

    Implement Dijkstra controller and service

---
+++ AlgoVisualizer/Services/AlgoVisualizer.Services/Interfaces/IDijkstraService.cs
@@ -0,0 +1,10 @@
+﻿namespace AlgoVisualizer.Services.Interfaces
+{
+    using Models;
+    using Models.Dijkstra;
+
+    public interface IDijkstraService
+    {
+        Result FindPath(DijkstraServiceModel model);
+    }
+}
