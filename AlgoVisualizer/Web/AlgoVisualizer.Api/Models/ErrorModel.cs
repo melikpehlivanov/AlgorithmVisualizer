c0ba31acfe1dac56f4ef5066d3dcf149f0dc27bc
Melik Pehlivanov <melikpehlivanov@gmail.com>
2020-01-25 15:33:51 +0200

    Add ErrorModel and return error model with given message in case of error

---
+++ AlgoVisualizer/Web/AlgoVisualizer.Api/Models/ErrorModel.cs
@@ -0,0 +1,26 @@
+﻿namespace AlgoVisualizer.Api.Models
+{
+    using System.Collections.Generic;
+
+    public class ErrorModel
+    {
+        public bool IsSuccess => false;
+
+        public List<string> Messages { get; }
+
+        public ErrorModel(List<string> messages)
+        {
+            this.Messages = messages ?? new List<string>();
+        }
+
+        public ErrorModel(string message)
+        {
+            this.Messages = new List<string>();
+
+            if (!string.IsNullOrWhiteSpace(message))
+            {
+                this.Messages.Add(message);
+            }
+        }
+    }
+}
