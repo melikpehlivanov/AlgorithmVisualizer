0df9622398242bc8013be7f6db65c89c4d227275
Melik Pehlivanov <melikpehlivanov@gmail.com>
2020-01-26 13:00:08 +0200

    Add GridProvider in GridLayout

--- AlgoVisualizer/Web/ClientApp/src/components/Layouts/GridLayout/GridLayout.jsx
+++ AlgoVisualizer/Web/ClientApp/src/components/Layouts/GridLayout/GridLayout.jsx
@@ -2,47 +2,50 @@ import React, { Component, Fragment } from 'react';
 import './GridLayout.css';
 import GridNavbar from '../../Navbar/GridNavbar';
 import Error from '../../Error';
+import { GridProvider } from '../../../store/context/gridContext';
 
 export class GridLayout extends Component {
   render() {
     return (
-      <Fragment>
-        <GridNavbar />
-        <Error />
-        <div id="algo-legend">
-          <ul>
-            <li>
-              <div className="node node-start"></div>
-              Start Node
-            </li>
-            <li>
-              <div className="node node-end"></div>
-              End Node
-            </li>
-            <li>
-              <div className="node node-wall"></div>
-              Wall Node
-            </li>
-            <li>
-              <i className="dumbbell"></i>
-              Weight Node
-            </li>
-            <li>
-              <div className="node"></div>
-              Unvisited Node
-            </li>
-            <li>
-              <div className="node node-visited"></div>
-              Visited Node
-            </li>
-            <li>
-              <div className="node node-shortest-path"></div>
-              Shortest Path Node
-            </li>
-          </ul>
-        </div>
-        <div className="main-grid">{this.props.children}</div>
-      </Fragment>
+      <GridProvider>
+        <Fragment>
+          <GridNavbar />
+          <Error />
+          <div id="algo-legend">
+            <ul>
+              <li>
+                <div className="node node-start"></div>
+                Start Node
+              </li>
+              <li>
+                <div className="node node-end"></div>
+                End Node
+              </li>
+              <li>
+                <div className="node node-wall"></div>
+                Wall Node
+              </li>
+              <li>
+                <i className="dumbbell"></i>
+                Weight Node
+              </li>
+              <li>
+                <div className="node"></div>
+                Unvisited Node
+              </li>
+              <li>
+                <div className="node node-visited"></div>
+                Visited Node
+              </li>
+              <li>
+                <div className="node node-shortest-path"></div>
+                Shortest Path Node
+              </li>
+            </ul>
+          </div>
+          <div className="main-grid">{this.props.children}</div>
+        </Fragment>
+      </GridProvider>
     );
   }
 }
