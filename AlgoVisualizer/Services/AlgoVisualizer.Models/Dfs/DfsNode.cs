1fd64c7c32fcf9b0ee084eb6dc2fdefe7f8b837b
Melik Pehlivanov <melikpehlivanov@gmail.com>
2020-01-28 19:39:08 +0200

    Implement Equals method in DfsNode

--- AlgoVisualizer/Services/AlgoVisualizer.Models/Dfs/DfsNode.cs
+++ AlgoVisualizer/Services/AlgoVisualizer.Models/Dfs/DfsNode.cs
@@ -17,6 +17,17 @@
 
         public NodeType? NodeType { get; set; }
 
+        public override bool Equals(object obj)
+            => obj is DfsNode other && (this.Row == other.Row && this.Col == other.Col);
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (this.Row * 397) ^ this.Col;
+            }
+        }
+
         public int CompareTo(object obj)
             => 0;
     }
