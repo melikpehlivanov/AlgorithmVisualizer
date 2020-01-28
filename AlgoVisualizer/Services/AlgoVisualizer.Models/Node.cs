9a50b9fcca57bdd23537d78ba24e4db1472a6fb3
Melik Pehlivanov <melikpehlivanov@gmail.com>
2020-01-28 19:17:27 +0200

    Create Node class in service models

---
+++ AlgoVisualizer/Services/AlgoVisualizer.Models/Node.cs
@@ -0,0 +1,22 @@
+﻿namespace AlgoVisualizer.Models
+{
+    using Enums;
+
+    public class Node : INode
+    {
+        public int Row { get; set; }
+
+        public int Col { get; set; }
+
+        public double Distance { get; set; }
+
+        public bool IsVisited { get; set; }
+
+        public INode PreviousNode { get; set; }
+
+        public NodeType? NodeType { get; set; }
+
+        public int CompareTo(object obj)
+            => 0;
+    }
+}
