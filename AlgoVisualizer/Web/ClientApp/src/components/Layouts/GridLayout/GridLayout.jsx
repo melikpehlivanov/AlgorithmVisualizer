3f1b3ede1690ec7839accf00c18adad55a66a5b4
Melik Pehlivanov <melikpehlivanov@gmail.com>
2020-01-30 17:32:06 +0200

    Add first page of modal in GridLayout component

--- AlgoVisualizer/Web/ClientApp/src/components/Layouts/GridLayout/GridLayout.jsx
+++ AlgoVisualizer/Web/ClientApp/src/components/Layouts/GridLayout/GridLayout.jsx
@@ -1,17 +1,55 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import './GridLayout.css';
 import GridNavbar from '../../Navbar/GridNavbar';
 import Error from '../../Error';
 import { GridProvider } from '../../../store/context/gridContext';
 import { ErrorProvider } from '../../../store/context/errorContext';
+import VerticallyCenteredModal from '../../Modal/VerticallyCenteredModal';
+
+const modalContent = [
+  {
+    title: 'Welcome to Algorithm Visualizer!',
+    body: {
+      __html:
+        '<h4>This short tutorial will walk you through the basic features of the application and will show you how to use them.</h4><p>If you want to wade right in, feel free to press the "Skip Tutorial" button below. Otherwise, press "Next"!</p>'
+    }
+  }
+];
 
 export const GridLayout = props => {
+  const [modalShow, setModalShow] = useState(false);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  console.log(pageNumber);
+  useEffect(() => {
+    setModalShow(true);
+  }, []);
   return (
     <GridProvider>
       <ErrorProvider>
         <Fragment>
           <GridNavbar />
           <Error />
+          {modalShow ? (
+            <VerticallyCenteredModal
+              show={modalShow}
+              title={modalContent[pageNumber - 1].title}
+              body={modalContent[pageNumber - 1].body}
+              onHide={() => setModalShow(false)}
+              previousPage={() =>
+                pageNumber > 1 ? setPageNumber(pageNumber - 1) : null
+              }
+              nextPage={() =>
+                pageNumber <= modalContent.length - 1
+                  ? setPageNumber(pageNumber + 1)
+                  : null
+              }
+              currentPage={pageNumber}
+              totalPages={modalContent.length}
+            />
+          ) : (
+            ''
+          )}
           <div id="algo-legend">
             <ul>
               <li>
@@ -27,7 +65,11 @@ export const GridLayout = props => {
                 Wall Node
               </li>
               <li>
-                <img className="dumbbell" src="dumbbell-solid.svg" />
+                <img
+                  className="dumbbell"
+                  src="dumbbell-solid.svg"
+                  alt="weight-node"
+                />
                 Weight Node
               </li>
               <li>
