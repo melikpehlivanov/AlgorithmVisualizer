2a3cfbe87f04809ee683df1737092db415b20ba6
Melik Pehlivanov <melikpehlivanov@gmail.com>
2020-01-30 20:10:22 +0200

    Convert Node component to functional

--- AlgoVisualizer/Web/ClientApp/src/components/Grid/Node/Node.jsx
+++ AlgoVisualizer/Web/ClientApp/src/components/Grid/Node/Node.jsx
@@ -1,29 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import './Node.css';
 
-export default class Node extends Component {
-  render() {
-    const { row, col, isStart, isEnd, isWall, isWeight } = this.props.node;
-    const extraClassName = isStart
-      ? 'node-start'
-      : isEnd
-      ? 'node-end'
-      : isWall
-      ? 'node-wall'
-      : '';
+const Node = props => {
+  const { row, col, isStart, isEnd, isWall, isWeight } = props.node;
+  const extraClassName = isStart
+    ? 'node-start'
+    : isEnd
+    ? 'node-end'
+    : isWall
+    ? 'node-wall'
+    : '';
 
-    return (
-      <div
-        id={`node-${row}-${col}`}
-        className={extraClassName ? `node ${extraClassName}` : 'node'}
-        onClick={event => this.props.onClick(event, row, col)}
-        onMouseOver={event => this.props.onMouseOver(event, row, col)}
-        onMouseDown={() => this.props.onMouseDown()}
-        onMouseUp={() => this.props.onMouseUp()}
-      >
-        {isWeight ? <i className="node-dumbbell"></i> : ''}
-      </div>
-    );
-  }
-}
+  return (
+    <div
+      id={`node-${row}-${col}`}
+      className={extraClassName ? `node ${extraClassName}` : 'node'}
+      onClick={event => props.onClick(event, row, col)}
+      onMouseOver={event => props.onMouseOver(event, row, col)}
+      onMouseDown={props.onMouseDown}
+      onMouseUp={props.onMouseUp}
+    >
+      {isWeight ? <i className="node-dumbbell"></i> : ''}
+    </div>
+  );
+};
+
+export default Node;
