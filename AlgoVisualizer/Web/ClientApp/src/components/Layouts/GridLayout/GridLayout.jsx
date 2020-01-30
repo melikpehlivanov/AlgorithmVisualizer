55d7dac28a02d0c12e4cbc0bb0c4c59ed787c2de
Melik Pehlivanov <melikpehlivanov@gmail.com>
2020-01-30 11:17:01 +0200

    Convert class based layout components to functional

--- AlgoVisualizer/Web/ClientApp/src/components/Layouts/GridLayout/GridLayout.jsx
+++ AlgoVisualizer/Web/ClientApp/src/components/Layouts/GridLayout/GridLayout.jsx
@@ -1,54 +1,52 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment } from 'react';
 import './GridLayout.css';
 import GridNavbar from '../../Navbar/GridNavbar';
 import Error from '../../Error';
 import { GridProvider } from '../../../store/context/gridContext';
 import { ErrorProvider } from '../../../store/context/errorContext';
 
-export class GridLayout extends Component {
-  render() {
-    return (
-      <GridProvider>
-        <ErrorProvider>
-          <Fragment>
-            <GridNavbar />
-            <Error />
-            <div id="algo-legend">
-              <ul>
-                <li>
-                  <div className="node node-start"></div>
-                  Start Node
-                </li>
-                <li>
-                  <div className="node node-end"></div>
-                  End Node
-                </li>
-                <li>
-                  <div className="node node-wall"></div>
-                  Wall Node
-                </li>
-                <li>
-                  <i className="dumbbell"></i>
-                  Weight Node
-                </li>
-                <li>
-                  <div className="node"></div>
-                  Unvisited Node
-                </li>
-                <li>
-                  <div className="node node-visited"></div>
-                  Visited Node
-                </li>
-                <li>
-                  <div className="node node-shortest-path"></div>
-                  Shortest Path Node
-                </li>
-              </ul>
-            </div>
-            <div className="main-grid">{this.props.children}</div>
-          </Fragment>
-        </ErrorProvider>
-      </GridProvider>
-    );
-  }
-}
+export const GridLayout = props => {
+  return (
+    <GridProvider>
+      <ErrorProvider>
+        <Fragment>
+          <GridNavbar />
+          <Error />
+          <div id="algo-legend">
+            <ul>
+              <li>
+                <div className="node node-start"></div>
+                Start Node
+              </li>
+              <li>
+                <div className="node node-end"></div>
+                End Node
+              </li>
+              <li>
+                <div className="node node-wall"></div>
+                Wall Node
+              </li>
+              <li>
+                <i className="dumbbell"></i>
+                Weight Node
+              </li>
+              <li>
+                <div className="node"></div>
+                Unvisited Node
+              </li>
+              <li>
+                <div className="node node-visited"></div>
+                Visited Node
+              </li>
+              <li>
+                <div className="node node-shortest-path"></div>
+                Shortest Path Node
+              </li>
+            </ul>
+          </div>
+          <div className="main-grid">{props.children}</div>
+        </Fragment>
+      </ErrorProvider>
+    </GridProvider>
+  );
+};
