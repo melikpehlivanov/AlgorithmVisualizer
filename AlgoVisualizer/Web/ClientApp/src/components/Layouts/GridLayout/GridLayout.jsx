c9a4f3ed5ada5d8b1b2f5a82c471b32c794690a8
Melik Pehlivanov <melikpehlivanov@gmail.com>
2020-01-30 17:35:08 +0200

    Add second page in modal

--- AlgoVisualizer/Web/ClientApp/src/components/Layouts/GridLayout/GridLayout.jsx
+++ AlgoVisualizer/Web/ClientApp/src/components/Layouts/GridLayout/GridLayout.jsx
@@ -13,6 +13,13 @@ const modalContent = [
       __html:
         '<h4>This short tutorial will walk you through the basic features of the application and will show you how to use them.</h4><p>If you want to wade right in, feel free to press the "Skip Tutorial" button below. Otherwise, press "Next"!</p>'
     }
+  },
+  {
+    title: 'What is a pathfinding algorithm ?',
+    body: {
+      __html:
+        '<h4>At its core, a pathfinding algorithm searches a graph/grid by starting at one vertex/node and exploring adjacent nodes until the destination node is reached, generally with the intent of finding the cheapest route.</h4><p>All the algorithms for this application are adapted for a 2D grid, where 90 degree turns have a cost of 1, and movements from node to node have cost of 1 again.</p>'
+    }
   }
 ];
 
@@ -20,10 +27,10 @@ export const GridLayout = props => {
   const [modalShow, setModalShow] = useState(false);
   const [pageNumber, setPageNumber] = useState(1);
 
-  console.log(pageNumber);
   useEffect(() => {
     setModalShow(true);
   }, []);
+
   return (
     <GridProvider>
       <ErrorProvider>
