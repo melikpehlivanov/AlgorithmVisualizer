8b3f821d510669c3c5f2744f26f80253c38146f6
Melik Pehlivanov <melikpehlivanov@gmail.com>
2020-01-30 20:21:35 +0200

    Convert Home component to functional

--- AlgoVisualizer/Web/ClientApp/src/components/Home.jsx
+++ AlgoVisualizer/Web/ClientApp/src/components/Home.jsx
@@ -1,23 +1,21 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 
-export class Home extends Component {
-  render() {
-    return (
-      <Fragment>
-        <div>
-          <h1>Welcome to AlgoVisualizer!</h1>
-          <p>Please choose one of the following algorithms</p>
-          <li>
-            <ul>
-              <Link to="/pathfindingAlgorithms">Pathfinding algorithms</Link>
-            </ul>
-            <ul>
-              <p>Sorting algorithms - Currently being developed...</p>
-            </ul>
-          </li>
-        </div>
-      </Fragment>
-    );
-  }
-}
+export const Home = () => {
+  return (
+    <Fragment>
+      <div>
+        <h1>Welcome to AlgoVisualizer!</h1>
+        <p>Please choose one of the following algorithms</p>
+        <li>
+          <ul>
+            <Link to="/pathfindingAlgorithms">Pathfinding algorithms</Link>
+          </ul>
+          <ul>
+            <p>Sorting algorithms - coming soon...</p>
+          </ul>
+        </li>
+      </div>
+    </Fragment>
+  );
+};
