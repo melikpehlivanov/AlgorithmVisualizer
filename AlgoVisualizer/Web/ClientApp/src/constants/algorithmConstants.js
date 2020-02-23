d8c757e7e84803eee5138d1d8ab7d933bf115173
Melik Pehlivanov <melikpehlivanov@gmail.com>
2020-02-23 10:51:12 +0200

    Add api url for sorting algorithms

--- AlgoVisualizer/Web/ClientApp/src/constants/algorithmConstants.js
+++ AlgoVisualizer/Web/ClientApp/src/constants/algorithmConstants.js
@@ -30,3 +30,6 @@ export const availableAlgorithms = [
 
 export const PATHFINDING_ALGORITHMS_API_URL =
   'https://localhost:44370/api/pathfinding';
+
+export const SORTING_ALGORITHMS_API_URL =
+  'https://localhost:44370/api/sortingAlgorithms';
