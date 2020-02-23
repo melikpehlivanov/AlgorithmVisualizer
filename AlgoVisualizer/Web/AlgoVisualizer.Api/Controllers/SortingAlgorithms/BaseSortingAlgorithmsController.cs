5ecc977eaa7351b2b7fe6715022d5613f4bda46d
Melik Pehlivanov <melikpehlivanov@gmail.com>
2020-02-23 08:24:21 +0200

    Add BaseSortingAlgorithmsController

---
+++ AlgoVisualizer/Web/AlgoVisualizer.Api/Controllers/SortingAlgorithms/BaseSortingAlgorithmsController.cs
@@ -0,0 +1,11 @@
+﻿namespace AlgoVisualizer.Api.Controllers.SortingAlgorithms
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiController]
+    [Produces("application/json")]
+    [Route("api/sortingAlgorithms/[controller]")]
+    public class BaseSortingAlgorithmsController : ControllerBase
+    {
+    }
+}
