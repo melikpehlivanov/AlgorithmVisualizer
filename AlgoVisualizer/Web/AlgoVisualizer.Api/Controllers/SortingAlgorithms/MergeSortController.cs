e5386ebb839bfb7a575db78d54180616dd9092ba
Melik Pehlivanov <melikpehlivanov@gmail.com>
2020-02-29 22:08:52 +0200

    Implement merge sort algorithm

---
+++ AlgoVisualizer/Web/AlgoVisualizer.Api/Controllers/SortingAlgorithms/MergeSortController.cs
@@ -0,0 +1,28 @@
+﻿namespace AlgoVisualizer.Api.Controllers.SortingAlgorithms
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Models;
+    using Services.SortingAlgorithms.Interfaces;
+
+    public class MergeSortController : BaseSortingAlgorithmsController
+    {
+        private readonly IMergeSortService mergeSortService;
+
+        public MergeSortController(IMergeSortService mergeSortService)
+        {
+            this.mergeSortService = mergeSortService;
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] SortingAlgorithmsRequestModel data)
+        {
+            var result = this.mergeSortService.Sort(data.Array);
+            if (result.ErrorMessage != null)
+            {
+                return this.BadRequest(new ErrorModel(result.ErrorMessage));
+            }
+
+            return this.Ok(result.SwapIndexes);
+        }
+    }
+}
