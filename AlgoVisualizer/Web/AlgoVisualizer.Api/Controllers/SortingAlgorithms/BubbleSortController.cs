cb33025ccde1e76115a9baaf1e3866f9d386a84f
Melik Pehlivanov <melikpehlivanov@gmail.com>
2020-03-01 11:15:06 +0200

    Implement bubble sort controller

---
+++ AlgoVisualizer/Web/AlgoVisualizer.Api/Controllers/SortingAlgorithms/BubbleSortController.cs
@@ -0,0 +1,29 @@
+﻿namespace AlgoVisualizer.Api.Controllers.SortingAlgorithms
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Models;
+    using Services.SortingAlgorithms.Interfaces;
+
+    public class BubbleSortController : BaseSortingAlgorithmsController
+    {
+        private readonly IBubbleSortService bubbleSortService;
+
+        public BubbleSortController(IBubbleSortService bubbleSortService)
+        {
+            this.bubbleSortService = bubbleSortService;
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] SortingAlgorithmsRequestModel data)
+        {
+            var result = this.bubbleSortService.Sort(data.Array);
+
+            if (result.ErrorMessage != null)
+            {
+                return this.BadRequest(new ErrorModel(result.ErrorMessage));
+            }
+
+            return this.Ok(result.SwapIndexes);
+        }
+    }
+}
