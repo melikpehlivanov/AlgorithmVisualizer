e6c39b79a97693bbddfee56e3b9e30d15585b38b
Melik Pehlivanov <melikpehlivanov@gmail.com>
2020-03-06 18:58:58 +0200

    Implement MazeService

---
+++ AlgoVisualizer/Services/AlgoVisualizer.Services/IMazeService.cs
@@ -0,0 +1,12 @@
+﻿namespace AlgoVisualizer.Services
+{
+    using System.Collections.Generic;
+    using Models.Maze;
+
+    public interface IMazeService
+    {
+        List<int[]> GenerateWallMaze(MazeServiceModel model);
+
+        List<int[]> GenerateWeightMaze(MazeServiceModel model);
+    }
+}
