61df681f5ae7dd0cc4ed76b49f42096346414586
Melik Pehlivanov <melikpehlivanov@gmail.com>
2020-03-28 17:37:02 +0200

    Set total swaps to default value before fetching data from api

--- AlgoVisualizer/Web/ClientApp/src/components/SortingAlgorithmsVisualizer/NavBar/index.jsx
+++ AlgoVisualizer/Web/ClientApp/src/components/SortingAlgorithmsVisualizer/NavBar/index.jsx
@@ -21,6 +21,7 @@ import { SORTING_ALGORITHMS_API_URL } from '../../../constants/algorithmConstant
 import { clearErrors, showError } from '../../../store/error/actions';
 import { makePostApiCallAsync } from '../../../helpers/fetchData';
 import { visualizeArrayElementsSwapping } from '../../../helpers/sortingAlgorithmsHelper';
+import { TOTAL_SWAPS_DEFAULT_VALUE } from '../../../constants/sortingAlgorithmsConstants';
 
 export const SortingAlgorithmsNavbar = () => {
   const [algorithm, setAlgorithm] = useState('');
@@ -30,6 +31,7 @@ export const SortingAlgorithmsNavbar = () => {
 
   const fetchData = async () => {
     dispatchError(clearErrors());
+    dispatch(setTotalSwaps(TOTAL_SWAPS_DEFAULT_VALUE));
 
     const url = `${SORTING_ALGORITHMS_API_URL}/${algorithm.value}`;
     dispatch(setIsNavbarClickable(false));
--- AlgoVisualizer/Web/ClientApp/src/store/sortingAlgorithms/context/index.js
+++ AlgoVisualizer/Web/ClientApp/src/store/sortingAlgorithms/context/index.js
@@ -1,12 +1,13 @@
 import React, { createContext, useReducer } from 'react';
 import sortingAlgorithmsReducer from '../reducers';
 import { availableSortingAlgorithms } from '../../../constants/algorithmConstants';
+import { TOTAL_SWAPS_DEFAULT_VALUE } from '../../../constants/sortingAlgorithmsConstants';
 
 export const initialState = {
   isLoading: true,
   isNavbarClickable: true,
   algorithms: availableSortingAlgorithms,
-  totalSwaps: '',
+  totalSwaps: TOTAL_SWAPS_DEFAULT_VALUE,
   barChart: {
     labels: [],
     datasets: [
