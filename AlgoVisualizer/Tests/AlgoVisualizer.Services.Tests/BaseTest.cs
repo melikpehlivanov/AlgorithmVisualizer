4bf1ed9a13fc44a91c10957cf95397dfb087a758
Melik Pehlivanov <melikpehlivanov@gmail.com>
2020-03-28 18:22:32 +0200

    Add assertion for Sort_With_AlreadySortedData in BubbleSortService

---
+++ AlgoVisualizer/Tests/AlgoVisualizer.Services.Tests/BaseTest.cs
@@ -0,0 +1,33 @@
+﻿namespace AlgoVisualizer.Services.Tests
+{
+    using System;
+
+    public abstract class BaseTest
+    {
+        private readonly Random random = new Random();
+
+        protected int[] GenerateSortedArray(int count)
+        {
+            var data = new int[count];
+
+            for (var i = 0; i < count; i++)
+            {
+                data[i] = i;
+            }
+
+            return data;
+        }
+
+        protected int[] GenerateRandomArray(int count)
+        {
+            var data = new int[count];
+
+            for (var i = 0; i < count; i++)
+            {
+                data[i] = this.random.Next(1, 1500);
+            }
+
+            return data;
+        }
+    }
+}
---
+++ AlgoVisualizer/Tests/AlgoVisualizer.Services.Tests/SortingAlgorithmsTests/BubbleSortServiceTests.cs
@@ -0,0 +1,37 @@
+﻿namespace AlgoVisualizer.Services.Tests.SortingAlgorithmsTests
+{
+    using Common;
+    using FluentAssertions;
+    using Models.SortingAlgorithms;
+    using SortingAlgorithms.Implementations;
+    using SortingAlgorithms.Interfaces;
+    using Xunit;
+
+    public class BubbleSortServiceTests : BaseTest
+    {
+        private readonly IBubbleSortService bubbleSortService;
+
+        public BubbleSortServiceTests()
+        {
+            this.bubbleSortService = new BubbleSortService();
+        }
+
+        [Fact]
+        public void Sort_With_AlreadySortedData_Should_Return_DataAlreadySortedErrorMessage()
+        {
+            // Arrange
+            const int arrayLength = 50;
+            var sortedData = this.GenerateSortedArray(arrayLength);
+
+            // Act
+            var result = this.bubbleSortService.Sort(sortedData);
+
+            // Assert
+            result
+                .Should()
+                .Match(x => x.As<Result>().ErrorMessage != null)
+                .And
+                .Match(x => x.As<Result>().ErrorMessage == NotificationMessages.SortingAlgorithms.DataAlreadySortedErrorMessage);
+        }
+    }
+}
