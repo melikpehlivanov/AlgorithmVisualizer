1546ec39eda6f999d402f21411af0f4774c2b774
Melik Pehlivanov <melikpehlivanov@gmail.com>
2020-03-28 20:10:55 +0200

    Add Sort_With_AlreadySortedData_Should_Return_DataAlreadySortedErrorMessage() assertion for MergeSortService

---
+++ AlgoVisualizer/Tests/AlgoVisualizer.Services.Tests/SortingAlgorithmsTests/MergeSortServiceTests.cs
@@ -0,0 +1,37 @@
+﻿namespace AlgoVisualizer.Services.Tests.SortingAlgorithmsTests
+{
+    using Common;
+    using FluentAssertions;
+    using Models.SortingAlgorithms;
+    using SortingAlgorithms.Implementations;
+    using SortingAlgorithms.Interfaces;
+    using Xunit;
+
+    public class MergeSortServiceTests : BaseTest
+    {
+        private readonly IMergeSortService mergeSortService;
+
+        public MergeSortServiceTests()
+        {
+            this.mergeSortService = new MergeSortService();
+        }
+
+        [Fact]
+        public void Sort_With_AlreadySortedData_Should_Return_DataAlreadySortedErrorMessage()
+        {
+            // Arrange
+            const int arrayLength = 49;
+            var sortedData = this.GenerateSortedArray(arrayLength);
+
+            // Act
+            var result = this.mergeSortService.Sort(sortedData);
+
+            // Assert
+            result
+                .Should()
+                .Match(x => x.As<Result>().ErrorMessage != null)
+                .And
+                .Match(x => x.As<Result>().ErrorMessage == NotificationMessages.SortingAlgorithms.DataAlreadySortedErrorMessage);
+        }
+    }
+}
