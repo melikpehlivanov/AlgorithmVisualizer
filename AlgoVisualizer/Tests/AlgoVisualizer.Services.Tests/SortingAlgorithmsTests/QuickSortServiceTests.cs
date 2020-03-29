e813261817da29920e0305c4b45a07fc75a38130
Melik Pehlivanov <melikpehlivanov@gmail.com>
2020-03-29 12:14:39 +0300

    Add Sort_With_AlreadySortedData_Should_Return_DataAlreadySortedErrorMessage assertion for QuickSortService

---
+++ AlgoVisualizer/Tests/AlgoVisualizer.Services.Tests/SortingAlgorithmsTests/QuickSortServiceTests.cs
@@ -0,0 +1,37 @@
+﻿namespace AlgoVisualizer.Services.Tests.SortingAlgorithmsTests
+{
+    using Common;
+    using FluentAssertions;
+    using Models.SortingAlgorithms;
+    using SortingAlgorithms.Implementations;
+    using SortingAlgorithms.Interfaces;
+    using Xunit;
+
+    public class QuickSortServiceTests : BaseTest
+    {
+        private readonly IQuickSortService quickSortService;
+
+        public QuickSortServiceTests()
+        {
+            this.quickSortService = new QuickSortService();
+        }
+
+        [Fact]
+        public void Sort_With_AlreadySortedData_Should_Return_DataAlreadySortedErrorMessage()
+        {
+            // Arrange
+            const int arrayLength = 49;
+            var sortedData = this.GenerateSortedArray(arrayLength);
+
+            // Act
+            var result = this.quickSortService.Sort(sortedData);
+
+            // Assert
+            result
+                .Should()
+                .Match(x => x.As<Result>().ErrorMessage != null)
+                .And
+                .Match(x => x.As<Result>().ErrorMessage == NotificationMessages.SortingAlgorithms.DataAlreadySortedErrorMessage);
+        }
+    }
+}
