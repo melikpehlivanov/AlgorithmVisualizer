97da56228a0e716091d6f2eadb17c5721b9ecc21
Melik Pehlivanov <melikpehlivanov@gmail.com>
2020-03-29 16:01:36 +0300

    Add Sort_With_AlreadySortedData_Should_Return_DataAlreadySortedErrorMessage assertion for SelectionSortService

---
+++ AlgoVisualizer/Tests/AlgoVisualizer.Services.Tests/SortingAlgorithmsTests/SelectionSortServiceTests.cs
@@ -0,0 +1,38 @@
+﻿namespace AlgoVisualizer.Services.Tests.SortingAlgorithmsTests
+{
+    using System;
+    using Common;
+    using FluentAssertions;
+    using Models.SortingAlgorithms;
+    using SortingAlgorithms.Implementations;
+    using SortingAlgorithms.Interfaces;
+    using Xunit;
+
+    public class SelectionSortServiceTests : BaseTest
+    {
+        private readonly ISelectionSortService selectionSortService;
+
+        public SelectionSortServiceTests()
+        {
+            this.selectionSortService = new SelectionSortService();
+        }
+
+        [Fact]
+        public void Sort_With_AlreadySortedData_Should_Return_DataAlreadySortedErrorMessage()
+        {
+            // Arrange
+            const int arrayLength = 10000;
+            var sortedData = this.GenerateSortedArray(arrayLength);
+
+            // Act
+            var result = this.selectionSortService.Sort(sortedData);
+
+            // Assert
+            result
+                .Should()
+                .Match(x => x.As<Result>().ErrorMessage != null)
+                .And
+                .Match(x => x.As<Result>().ErrorMessage == NotificationMessages.SortingAlgorithms.DataAlreadySortedErrorMessage);
+        }
+    }
+}
