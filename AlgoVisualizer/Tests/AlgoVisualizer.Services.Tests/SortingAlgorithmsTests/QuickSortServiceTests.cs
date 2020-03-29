98ba48364e6cfb6b2a2fc53449e9ebc0167efd39
Melik Pehlivanov <melikpehlivanov@gmail.com>
2020-03-29 12:25:09 +0300

    Add Sort_With_ValidInput_Should_Return_SortedData assertion for QuickSortService

--- AlgoVisualizer/Tests/AlgoVisualizer.Services.Tests/SortingAlgorithmsTests/QuickSortServiceTests.cs
+++ AlgoVisualizer/Tests/AlgoVisualizer.Services.Tests/SortingAlgorithmsTests/QuickSortServiceTests.cs
@@ -108,5 +108,21 @@
                 .Should()
                 .Be(expectedTotalSwaps);
         }
+
+        [Fact]
+        public void Sort_With_ValidInput_Should_Return_SortedData()
+        {
+            // Arrange
+            const int arrayLength = 100_000_0;
+            var data = this.GenerateRandomArray(arrayLength);
+
+            // Act
+            this.quickSortService.Sort(data);
+
+            // Assert
+            data
+                .Should()
+                .BeInAscendingOrder();
+        }
     }
 }
