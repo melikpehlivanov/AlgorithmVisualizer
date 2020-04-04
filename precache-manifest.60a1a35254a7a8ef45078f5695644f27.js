869b72be2a104706f1979def12be5dd112c01599
Melik Pehlivanov <melikpehlivanov@gmail.com>
2020-04-04 17:38:11 +0300

    Updates

--- precache-manifest.60a1a35254a7a8ef45078f5695644f27.js
+++ precache-manifest.60a1a35254a7a8ef45078f5695644f27.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d33b06551a9e508a7d31f562111ab77f",
+    "revision": "55fd0a087d81f3a27d173f7b15032aa7",
     "url": "/AlgorithmVisualizer/index.html"
   },
   {
-    "revision": "a82ced23641f71b51109",
+    "revision": "5d262052c21cc6ff6f27",
     "url": "/AlgorithmVisualizer/static/css/2.67123b20.chunk.css"
   },
   {
-    "revision": "9ab8b73dd1748e357b72",
+    "revision": "199c4ccfc9007c7fad65",
     "url": "/AlgorithmVisualizer/static/css/main.67f6e225.chunk.css"
   },
   {
-    "revision": "a82ced23641f71b51109",
-    "url": "/AlgorithmVisualizer/static/js/2.c128da40.chunk.js"
+    "revision": "5d262052c21cc6ff6f27",
+    "url": "/AlgorithmVisualizer/static/js/2.db6cf95f.chunk.js"
   },
   {
     "revision": "de1258ae5739182d4cefb1b362c46404",
-    "url": "/AlgorithmVisualizer/static/js/2.c128da40.chunk.js.LICENSE"
+    "url": "/AlgorithmVisualizer/static/js/2.db6cf95f.chunk.js.LICENSE"
   },
   {
-    "revision": "9ab8b73dd1748e357b72",
-    "url": "/AlgorithmVisualizer/static/js/main.93e09496.chunk.js"
+    "revision": "199c4ccfc9007c7fad65",
+    "url": "/AlgorithmVisualizer/static/js/main.560e7e7d.chunk.js"
   },
   {
     "revision": "09a53ae46824c4a7034f",
