1872a7f4e5427249becd7f73c7f23e62b880aa86
Melik Pehlivanov <melikpehlivanov@gmail.com>
2020-04-04 20:54:05 +0300

    Add animation speed dropdown in pathfinding algorithms

--- AlgoVisualizer/Web/ClientApp/src/constants/pathfindingAlgorithms.js
+++ AlgoVisualizer/Web/ClientApp/src/constants/pathfindingAlgorithms.js
@@ -3,38 +3,55 @@ export const availablePathFindingAlgorithms = [
     value: 'astar',
     label: 'A* Search',
     description: 'A* Search is 𝐰𝐞𝐢𝐠𝐡𝐭𝐞𝐝 and 𝐠𝐮𝐚𝐫𝐚𝐧𝐭𝐞𝐞𝐬 the shortest path!',
-    isWeight: true
+    isWeight: true,
   },
   {
     value: 'dijkstra',
     label: "Dijkstra's Search",
     description:
       "Dijkstra's Algorithm is 𝐰𝐞𝐢𝐠𝐡𝐭𝐞𝐝 and 𝐠𝐮𝐚𝐫𝐚𝐧𝐭𝐞𝐞𝐬 the shortest path!",
-    isWeight: true
+    isWeight: true,
   },
   {
     value: 'bfs',
     label: 'Breadth-first Search',
     description:
       'Breath-first Search is 𝐮𝐧𝐰𝐞𝐢𝐠𝐡𝐭𝐞𝐝 and 𝐠𝐮𝐚𝐫𝐚𝐧𝐭𝐞𝐞𝐬 the shortest path!',
-    isWeight: false
+    isWeight: false,
   },
   {
     value: 'dfs',
     label: 'Depth-first Search',
     description:
       'Depth-first Search is 𝐮𝐧𝐰𝐞𝐢𝐠𝐡𝐭𝐞𝐝 and 𝐝𝐨𝐞𝐬 𝐧𝐨𝐭 𝐠𝐮𝐚𝐫𝐚𝐧𝐭𝐞𝐞 the shortest path!',
-    isWeight: false
-  }
+    isWeight: false,
+  },
 ];
 
 export const MAZE_TYPES = [
   {
     label: 'Generate wall maze',
-    value: 'wall'
+    value: 'wall',
   },
   {
     label: 'Generate weight maze',
-    value: 'weight'
-  }
+    value: 'weight',
+  },
+];
+
+export const DEFAULT_ANIMATION = {
+  label: 'Average',
+  value: 20,
+};
+
+export const ANIMATION_SPEEDS = [
+  {
+    label: 'Slow',
+    value: 120,
+  },
+  DEFAULT_ANIMATION,
+  {
+    label: 'Fast',
+    value: 10,
+  },
 ];
